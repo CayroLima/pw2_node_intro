63ec795e63ecc3cd929e15bdef35be8a8282a4b9
kauansiqueira-cps <kauan.siqueira2@etec.sp.gov.br>
2024-05-13 20:43:10 -0300

    Aula1305 - Controladores e Rotas de Get e Post

---
+++ Aula1305-ideiaapp/ideiaapp/db/conn.js
@@ -0,0 +1,18 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize('ideias_db', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql',
+})
+
+try {
+    sequelize.authenticate()
+    console.log('Conectado ao servidor MySQL!')
+} catch (error) {
+    console.error(`Error MySQL: ${error}`)
+}
+
+module.exports = sequelize
+
+
+
